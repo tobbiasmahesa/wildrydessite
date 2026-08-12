fbae4b1abbd0452136806cdbec86fcc4f3aa3c99
tobbiasmahesa <tobbiasmahesa@gmail.com>
2026-08-12 18:26:23 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_95kUSX8AT', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4c4kujplhepj4iqlgf90g6l4uj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
